959d5137781e8b6078188b50f92fd92af5a70a72
Austin Ellis <Austin@Janets-iMac.local>
2021-01-23 00:07:25 -0500

    started building components

---
+++ src/components/SearchBar/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const SearchBar = (props) => {
+    return (
+        <nav className="navbar navbar-light bg-light justify-content-center">
+            <form className="form-inline m-2">
+                <input 
+                    className="form-control"
+                    value={props.value}
+                    name="search"
+                    type="search"
+                    placeholder="Search"
+                    id="search"
+                />
+            </form>
+        </nav>
+    );
+};
+
+export default SearchBar;
