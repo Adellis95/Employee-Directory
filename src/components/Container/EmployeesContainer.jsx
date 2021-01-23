754718be540c4f8179a21e64470b5cc92557d448
Austin Ellis <Austin@Janets-iMac.local>
2021-01-23 00:45:21 -0500

    Built class component container with state

--- src/components/Container/EmployeesContainer.jsx
+++ src/components/Container/EmployeesContainer.jsx
@@ -1,11 +1,64 @@
 import React, { Component } from 'react';
+import SearchBar from "../SearchBar";
+import API from "../../pages/API";
 
 class EmployeesContainer extends Component {
+
+    state = {
+        search: "",
+        employees: [],
+        filteredEmployees: [],
+    };
+
+    componentDidMount() {
+        API.getEmployees()
+        .then((res) => this.setState({ employees: res.data.results }))
+        .catch((err) => console.log(err));
+    }
+
     render() {
         return (
-            <div>
-                
+        <>
+            <SearchBar />
+            <div className="container">
+                <table className="table table-striped text-center mt-5">
+                    <thead>
+                        <tr>
+                            <th scope="col">Image</th>
+                            <th scope="col">Nane</th>
+                            <th scope="col">Phone</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Date of Birth</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.employees.map((employee) => {
+                            const { first, last } = employee.name;
+                            const fullName = `${first} ${last}`;
+                            const date = new Date(employee.dob.date);
+                            let dob = [];
+
+                            dob.push(("0" + (date.getMonth() + 1)).slice(-2));
+                            dob.push(("0" + date.getDate()).slice(-2));
+                            dob.push(date.getFullYear());
+                            dob = dob.join("-");
+
+                            return (
+                                <tr>
+                                    <th scope="row">
+                                        <img src={employee.picture.thumbnail} alt={fullName} />
+                                    </th>
+                                    <td>{fullName}</td>
+                                    <td>{employee.cell}</td>
+                                    <td>{employee.email}</td>
+                                    <td>{dob}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
             </div>
+        </>
         );
     }
 }
