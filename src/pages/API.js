15225236a4715ef91243be43634f8ee1e255657e
Austin Ellis <Austin@Janets-iMac.attlocal.net>
2021-01-23 17:06:06 -0500

    Adjusted api url

--- src/pages/API.js
+++ src/pages/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "https://randomuser.me/api/?results=50"
+const URL = "https://randomuser.me/api/?results=50&nat=us"
 
 export default {
     getEmployees: function() {
