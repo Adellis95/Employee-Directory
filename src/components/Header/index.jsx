cfde1c57b25e74c985ca1d4a02e713f48d80c2d0
Austin Ellis <Austin@Janets-iMac.attlocal.net>
2021-01-23 20:49:29 -0500

    changed the summary of Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,6 +4,8 @@ const Header = () => {
     return (
         <header>
             <h1 className="text-center">Employee Directory</h1>
+            <p className="text-center">Click on column headers to 
+            filter by heading or use the search box to narrow your results.</p>
         </header>
     );
 };
